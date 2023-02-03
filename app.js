3f797c422f52463c0e595e76743e8cc9a9be6e02
Brady <brady.savarie@hotmail.com>
2023-02-02 22:13:55 -0500

    Write constructor and addBookToLibrary functions

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+const myLibrary = ['book1', 'book2'];
 
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    for (let i = 0; i <= myLibrary.length; i++) {
+        if (myLibrary[i] === undefined) {
+            myLibrary[i] = new Book(title, author, pages, read);
+            break;
+        }
+    }
+}
+
+addBookToLibrary('gameofthrones', 'hksomething', 143, true);
+
+console.log(myLibrary[2]);
