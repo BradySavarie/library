049ee7810171e3d7d40e4108101e7cdc7cf04092
Brady <brady.savarie@hotmail.com>
2023-02-06 19:10:46 -0500

    Include open and close form functionality

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const myLibrary = ['book1', 'book2'];
 
-const library = document.getElementById('library');
+const addBookForm = document.getElementById('addBookFormContainer');
+const overlay = document.getElementById('overlay');
+const openFormButton = document.getElementById('openFormButton');
+const closeFormButton = document.getElementById('closeFormButton');
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -18,10 +21,24 @@ function addBookToLibrary(title, author, pages, read) {
     }
 }
 
-function clearEmptyLibrary() {
-    library.innerHTML = '';
+function openAddBookForm() {
+    addBookForm.style.transform = 'translate(-50%, -50%) scale(1)';
+    overlay.style.display = 'block';
+}
+
+function closeAddBookForm() {
+    addBookForm.style.transform = 'translate(-50%, -50%) scale(0)';
+    overlay.style.display = 'none';
 }
 
 addBookToLibrary('gameofthrones', 'hksomething', 143, true);
 
 console.log(myLibrary[2]);
+
+openFormButton.addEventListener('click', () => {
+    openAddBookForm();
+});
+
+closeFormButton.addEventListener('click', () => {
+    closeAddBookForm();
+});
