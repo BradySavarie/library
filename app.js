0fc5f173f6ea37f07b9ecb9d0ea3b117ddcd731b
Brady <brady.savarie@hotmail.com>
2023-02-22 18:44:50 -0500

    Convert constructor to class

--- app.js
+++ app.js
@@ -12,11 +12,13 @@ const booksGrid = document.getElementById('booksGrid');
 
 // Functions //
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function openAddBookForm() {
